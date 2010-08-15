4cb16eb72ecc72ea8ef3f22ea5ace0a37ea44a68
Michael Lefebvre <michae@strict-edge.com>
2010-08-15 20:39:40 +0200

    add is_template_file, a function that test if $template is a file and then return the content as variable

--- system/application/libraries/ci_mustache.php
+++ system/application/libraries/ci_mustache.php
@@ -1,16 +1,15 @@
 <?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-
 require 'mustache/Mustache.php';
 
 class ci_mustache 
 {
 	private function get_include_contents($filename)
 	{
-		if (is_file($filename))
+		if(is_file($filename))
 		{
 			ob_start();
-			include $filename;
+			include($filename);
 			$contents = ob_get_contents();
 			ob_end_clean();
 			return $contents;
@@ -19,13 +18,26 @@ class ci_mustache
 		return false;
 	}
 	
+	private function is_template_file($template)
+	{	
+		$template_str = $this->get_include_contents($template);
+		
+		if($template_str)
+		{
+			return $template_str;
+		}
+		
+		return $template;
+	}
+	
 	public function render($template = null, $view = null, $partials = null)
 	{
-		/*
-		if ($template !== null) $this->_template = $template;
-		if ($partials !== null) $this->_partials = $partials;
-		if ($view !== null)     $this->_context = array($view);
-		*/
+		// inlude template file as var
+		//$template = $this->get_include_contents($template);
+		
+		// check if template is an available file
+		$template = $this->is_template_file($template);
+		
 		$m = new Mustache;
 		
 		return $m->render($template, $view, $partials);
